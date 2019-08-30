d2d47302401566ae1d948812ab54d8a3c6a762d6
Genadi <gim13@abv.bg>
2019-08-30 17:09:43 +0300

    New task

---
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/01. Basic Stack Operations/01.BasicStackOperations .cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
