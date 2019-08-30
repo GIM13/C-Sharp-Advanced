3b93e03762e384590fc67370060fcdb7f72dfb96
Genadi <gim13@abv.bg>
2019-08-30 20:44:05 +0300

    solution of the problem

--- C# Advanced - Exercises/Stacks and Queues - Exercise/02.BasicQueueOperations/02.BasicQueueOperations .cs
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/02.BasicQueueOperations/02.BasicQueueOperations .cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02.BasicQueueOperations
 {
@@ -6,7 +8,44 @@ namespace _02.BasicQueueOperations
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int numberEnqueue = input[0];
+            int numberDequeue = input[1];
+            int wantedElement = input[2];
+
+            int[] integers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < numberEnqueue; i++)
+            {
+                queue.Enqueue(integers[i]);
+            }
+            for (int i = 0; i < numberDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Contains(wantedElement))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                int minElement = 0;
+
+                if (queue.Count() > 0)
+                {
+                    minElement = queue.ToArray().Min();
+                }
+                Console.WriteLine(minElement);
+            }
         }
     }
 }
---
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
