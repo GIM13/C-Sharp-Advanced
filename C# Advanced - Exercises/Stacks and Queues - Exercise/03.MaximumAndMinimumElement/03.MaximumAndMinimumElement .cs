ccf0f4aca29dd1da4f3fd5b6778e1d41e40c8588
Genadi <gim13@abv.bg>
2019-08-30 21:34:34 +0300

    solution of the problem

---
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/03.MaximumAndMinimumElement/03.MaximumAndMinimumElement .cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberTimes = int.Parse(Console.ReadLine());
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < numberTimes; i++)
+            {
+                int[] command = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (command[0] == 1)
+                {
+                    stack.Push(command[1]);
+                }
+                else if (command[0] == 2 && stack.Count() > 0)
+                {
+                    stack.Pop();
+                }
+                else if (command[0] == 3 && stack.Count() > 0)
+                {
+                    int maxElement = stack.ToArray().Max();
+
+                    Console.WriteLine(maxElement);
+                }
+                else if (command[0] == 4 && stack.Count() > 0)
+                {
+                    int minElement = stack.ToArray().Min();
+
+                    Console.WriteLine(minElement);
+                }
+            }
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
