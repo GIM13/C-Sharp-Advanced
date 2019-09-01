4b3868940aea2640f2dfb9168184a18d261bfb1b
Genadi <gim13@abv.bg>
2019-09-01 18:10:03 +0300

    solution of  task 05 from  Stacks and Queues - Exercise

--- C# Advanced - Exercises/Stacks and Queues - Exercise/05FashionBoutique/05FashionBoutique .cs
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/05FashionBoutique/05FashionBoutique .cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05FashionBoutique
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int numberRack = 1;
+
+            var boxWithClothes = new Stack<int>();
+            Array.ForEach(input,x => boxWithClothes.Push(x));
+
+            int capacity = rackCapacity;
+
+            foreach (var garment in boxWithClothes)
+            {
+                if (capacity >= garment)
+                {
+                    capacity -= garment;
+                }
+                else
+                {
+                    capacity = rackCapacity;
+                    capacity -= garment;
+                    numberRack++;
+                }
+            }
+            Console.WriteLine(numberRack);
         }
     }
 }
