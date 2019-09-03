fae7ae9873b153f86563ef09b4ffb140eb619497
Genadi <gim13@abv.bg>
2019-09-03 19:37:34 +0300

    solution of  task 07 from  Stacks and Queues - Exercise

---
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/07TruckTour/07TruckTour .cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07TruckTour
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberPetrolPumps = int.Parse(Console.ReadLine());
+
+            var totalDistancePetrolPumps = new Queue<long>();
+            var totalAmountPetrol = new Queue<long>();
+
+            for (int i = 0; i < numberPetrolPumps; i++)
+            {
+                long[] input = Console.ReadLine()
+                    .Split()
+                    .Select(long.Parse)
+                    .ToArray();
+                totalAmountPetrol.Enqueue(input[0]);
+                totalDistancePetrolPumps.Enqueue(input[1]);
+            }
+            for (int i = 0; i < numberPetrolPumps; i++)
+            {
+                long availableAmountPetrol = totalAmountPetrol.Peek() - totalDistancePetrolPumps.Peek();
+                int indexPetrolPump = i;
+                int counter = 1;
+
+                while (availableAmountPetrol >= 0 && counter < numberPetrolPumps)
+                {
+                    totalAmountPetrol.Enqueue(totalAmountPetrol.Dequeue());
+                    totalDistancePetrolPumps.Enqueue(totalDistancePetrolPumps.Dequeue());
+
+                    availableAmountPetrol += totalAmountPetrol.Peek() - totalDistancePetrolPumps.Peek();
+                    counter++;
+                }
+                if (counter == numberPetrolPumps)
+                {
+                    Console.WriteLine(indexPetrolPump);
+                    break;
+                }
+                else
+                {
+                    for (int j = 0; j < numberPetrolPumps - counter + 2; j++)
+                    {
+                        totalAmountPetrol.Enqueue(totalAmountPetrol.Dequeue());
+                        totalDistancePetrolPumps.Enqueue(totalDistancePetrolPumps.Dequeue());
+                    }
+                }
+            }
+        }
+    }
+}
