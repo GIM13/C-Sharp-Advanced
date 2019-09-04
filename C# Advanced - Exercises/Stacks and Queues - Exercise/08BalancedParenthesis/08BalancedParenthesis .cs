795995da92991367cccc6d010d1bf53042110c88
Genadi <gim13@abv.bg>
2019-09-04 18:22:15 +0300

    solution of  task 08 from  Stacks and Queues - Exercise

---
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/08BalancedParenthesis/08BalancedParenthesis .cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08BalancedParenthesis
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] parentheses = Console.ReadLine().ToCharArray();
+
+            string result = "YES";
+            var parenthesesStack = new Stack<char>();
+
+            if (parentheses.Length % 2 != 0)
+            {
+                result = "NO";
+            }
+            else
+            {
+                foreach (var clamp in parentheses)
+                {
+                    if (clamp == '(' || clamp == '[' || clamp == '{')
+                    {
+                        parenthesesStack.Push(clamp);
+                    }
+                    else if (parenthesesStack.Count > 0)
+                    {
+                        if ((clamp == ')' && parenthesesStack.Pop() != '(')
+                         || (clamp == ']' && parenthesesStack.Pop() != '[')
+                         || (clamp == '}' && parenthesesStack.Pop() != '{'))
+                        {
+                            result = "NO";
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
