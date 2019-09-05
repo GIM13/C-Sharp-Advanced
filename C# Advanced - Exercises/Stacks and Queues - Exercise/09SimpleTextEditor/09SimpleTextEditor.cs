a2ccf2c93728614003db0723376117690cae65b4
Genadi <gim13@abv.bg>
2019-09-05 14:20:57 +0300

    solution of  task 09 from  Stacks and Queues - Exercise

---
+++ C# Advanced - Exercises/Stacks and Queues - Exercise/09SimpleTextEditor/09SimpleTextEditor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09SimpleTextEditor
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numOperations = int.Parse(Console.ReadLine());
+            var texts = new Stack<string>();
+            texts.Push(string.Empty);
+
+            for (int i = 0; i < numOperations; i++)
+            {
+                string[] operation = Console.ReadLine().Split();
+                string command = operation[0];
+                
+                if (command == "1")
+                {
+                    string valueCommand1 = operation[1];
+                    texts.Push(texts.Peek() + valueCommand1);
+                }
+                else if (command == "2")
+                {
+                    int valueCommand2 = int.Parse(operation[1]);
+                    string save = texts.Peek();
+                    int index = save.Length - valueCommand2;
+                    texts.Push(save.Remove(index));
+                }
+                else if (command == "3")
+                {
+                    int valueCommand3 = int.Parse(operation[1]);
+                    Console.WriteLine(texts.Peek()[valueCommand3 - 1]);
+                }
+                else if (command == "4")
+                {
+                    texts.Pop();
+                }
+            }
+        }
+    }
+}
