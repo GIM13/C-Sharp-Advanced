94f1a80115b1c479dac5aed8f05e41ac1253a998
Genadi <gim13@abv.bg>
2019-09-08 19:09:03 +0300

    solution of  tasks 01 and 02 from   Multidimensional Arrays - Exercise

---
+++ C# Advanced - Exercises/02 Multidimensional Arrays - Exercise/02SquaresInMatrix/02SquaresInMatrix.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _02SquaresInMatrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] matrixDimensions = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var matrix = new string[matrixDimensions[0], matrixDimensions[1]];
+
+            for (int row = 0; row < matrixDimensions[0]; row++)
+            {
+                string[] matrixRow = Console.ReadLine().Split();
+
+                for (int col = 0; col < matrixDimensions[1]; col++)
+                {
+                    matrix[row, col] = matrixRow[col];
+                }
+            }
+            int counter2x2Squares = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1]
+                     && matrix[row, col] == matrix[row + 1, col]
+                     && matrix[row, col] == matrix[row + 1, col + 1]
+                     )
+                    {
+                        counter2x2Squares++;
+                    }
+                }
+            }
+            Console.WriteLine(counter2x2Squares);
+        }
+    }
+}
---
+++ C# Advanced - Exercises/02 Multidimensional Arrays - Exercise/03MaximalSum/03MaximalSum.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace _03MaximalSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] matrixDimensions = Console.ReadLine()
+                 .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+            if (matrixDimensions[0] > 2 && matrixDimensions[1] > 2)
+            {
+                var matrix = new long[matrixDimensions[0], matrixDimensions[1]];
+
+                for (int row = 0; row < matrixDimensions[0]; row++)
+                {
+                    long[] matrixRow = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(long.Parse)
+                        .ToArray();
+                    for (int col = 0; col < matrixDimensions[1]; col++)
+                    {
+                        matrix[row, col] = matrixRow[col];
+                    }
+                }
+                long maxSum3x3Squares = long.MinValue;
+                int savaIndexsRow = -1;
+                int savaIndexsCol = -1;
+
+                for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                    {
+                        long sum3x3Squares = matrix[row, col]
+                                           + matrix[row, col + 1]
+                                           + matrix[row, col + 2]
+                                           + matrix[row + 1, col]
+                                           + matrix[row + 1, col + 1]
+                                           + matrix[row + 1, col + 2]
+                                           + matrix[row + 2, col]
+                                           + matrix[row + 2, col + 1]
+                                           + matrix[row + 2, col + 2];
+                        if (maxSum3x3Squares < sum3x3Squares)
+                        {
+                            maxSum3x3Squares = sum3x3Squares;
+                            savaIndexsRow = row;
+                            savaIndexsCol = col;
+                        }
+                    }
+                }
+                Console.WriteLine($"Sum = {maxSum3x3Squares}");
+
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine(matrix[savaIndexsRow + i, savaIndexsCol] + " "
+                                    + matrix[savaIndexsRow + i, savaIndexsCol  + 1] + " "
+                                    + matrix[savaIndexsRow + i, savaIndexsCol  + 2]);
+                }
+            }
+        }
+    }
+}
