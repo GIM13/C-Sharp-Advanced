6d107170e0dc47a41c8e3f9b778b54e6294dddc3
Genadi <gim13@abv.bg>
2019-09-08 13:42:12 +0300

    solution of  task 01 from   Multidimensional Arrays - Exercise

--- C# Advanced - Exercises/02 Multidimensional Arrays - Exercise/01DiagonalDifference/01DiagonalDifference.cs
+++ C# Advanced - Exercises/02 Multidimensional Arrays - Exercise/01DiagonalDifference/01DiagonalDifference.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01DiagonalDifference
 {
@@ -6,7 +7,29 @@ namespace _01DiagonalDifference
     {
         static void Main()
         {
-            
+            int sizeMatrix = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[sizeMatrix, sizeMatrix];
+            int sumPrimaryDagonal = 0;
+            int sumSecondaryDiagonal = 0;
+
+            for (int row = 0; row < sizeMatrix; row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < sizeMatrix; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            for (int i = 0; i < sizeMatrix; i++)
+            {
+                sumPrimaryDagonal += matrix[i, i];
+                sumSecondaryDiagonal += matrix[i, sizeMatrix - 1 - i];
+            }
+            Console.WriteLine(Math.Abs(sumPrimaryDagonal - sumSecondaryDiagonal));
         }
     }
 }
