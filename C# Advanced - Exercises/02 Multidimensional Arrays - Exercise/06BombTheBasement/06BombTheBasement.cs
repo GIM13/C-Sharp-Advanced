59cbded3530ead82ae8eaeb5d09f9c1e934cc4ca
Genadi <gim13@abv.bg>
2019-09-11 13:29:37 +0300

    solution of  task 06 from   Multidimensional Arrays - Exercise

--- C# Advanced - Exercises/02 Multidimensional Arrays - Exercise/06BombTheBasement/06BombTheBasement.cs
+++ C# Advanced - Exercises/02 Multidimensional Arrays - Exercise/06BombTheBasement/06BombTheBasement.cs
@@ -13,14 +13,42 @@ namespace _06BombTheBasement
                    .ToArray();
             int rows = matrixDimensions[0];
             int columns = matrixDimensions[1];
-            var matrix = new int[rows, columns];
+
+            int[] bombParameters = Console.ReadLine()
+                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                   .Select(int.Parse)
+                   .ToArray();
+            double rowsBomb = bombParameters[0];
+            double columnsBomb = bombParameters[1];
+            double radiusBomb = bombParameters[2];
+
+            var finalBasement = new int[rows][];
 
             for (int row = 0; row < rows; row++)
             {
+                finalBasement[row] = new int[columns];
+
                 for (int col = 0; col < columns; col++)
-                {
-                  
+                { 
+                    double differenceRow = Math.Abs(row - rowsBomb);
+                    double differenceCol = Math.Abs(col - columnsBomb);
+
+                    if (Math.Pow(differenceRow,2) + Math.Pow(differenceCol,2) <= Math.Pow(radiusBomb,2))
+                    {
+                        finalBasement[row][col] = 1;
+                    }
+                    else
+                    {
+                        finalBasement[row][col] = 0;
+                    }
                 }
+            }
+            finalBasement = finalBasement
+                .OrderByDescending(x => x.Sum())
+                .ToArray();
+            foreach (var row in finalBasement)
+            {
+                Console.WriteLine(string.Join("",row));
             }
         }
     }
