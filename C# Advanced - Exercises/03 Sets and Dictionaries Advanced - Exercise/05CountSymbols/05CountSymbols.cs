2e13d100ed91167683144fba55d15ebf239e9fd9
Genadi <gim13@abv.bg>
2019-09-11 22:05:31 +0300

    solution of  task 05 from   Multidimensional Arrays - Exercise

---
+++ C# Advanced - Exercises/03 Sets and Dictionaries Advanced - Exercise/05CountSymbols/05CountSymbols.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05CountSymbols
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            var charCounts = new Dictionary<char, int>();
+
+            foreach (var charr in text)
+            {
+                if (!charCounts.ContainsKey(charr))
+                {
+                    charCounts.Add(charr,0);
+                }
+                charCounts[charr]++;
+            }
+            charCounts = charCounts
+                .OrderBy(x => x.Key)
+                .ToDictionary(x => x.Key, y => y.Value);
+            foreach (var charr in charCounts)
+            {
+                Console.WriteLine($"{charr.Key}: {charr.Value} time/s");
+            }
+        }
+    }
+}
