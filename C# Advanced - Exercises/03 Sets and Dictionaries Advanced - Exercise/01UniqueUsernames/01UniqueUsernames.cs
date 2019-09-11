b236390629e41dcebe4964cdcc75e8e2a50f19b8
Genadi <gim13@abv.bg>
2019-09-11 19:41:35 +0300

    solution of  task 01 from   Sets and Dictionaries Advanced - Exercise

---
+++ C# Advanced - Exercises/03 Sets and Dictionaries Advanced - Exercise/01UniqueUsernames/01UniqueUsernames.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01UniqueUsernames
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            var names = new HashSet<string>();
+            for (int i = 0; i < num; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
