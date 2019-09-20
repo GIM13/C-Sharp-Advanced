161e1479a9ffc143ed3630d261a78522bbd030d8
Genadi <gim13@abv.bg>
2019-09-21 00:07:55 +0300

    return folders

---
+++ C# Advanced - Exercises/02 Multidimensional Arrays - Exercise/06JaggedArrayModification/06JaggedArrayModification.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace _06JaggedArrayModification
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numRow = int.Parse(Console.ReadLine());
+            double[][] rowCol = new double[numRow][];
+
+            for (int i = 0; i < numRow; i++)
+            {
+                rowCol[i] = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+                if (i > 0)
+                {
+                    if (rowCol[i].Count() == rowCol[i - 1].Count())
+                    {
+                        for (int j = 0; j < rowCol[i].Count(); j++)
+                        {
+                            rowCol[i][j] *= 2;
+                            rowCol[i - 1][j] *= 2;
+                        }
+                    }
+                    else
+                    {
+                        for (int j = 0; j < rowCol[i].Count(); j++)
+                        {
+                            rowCol[i][j] /= 2;
+                        }
+                        for (int j = 0; j < rowCol[i - 1].Count(); j++)
+                        {
+                            rowCol[i - 1][j] /= 2;
+                        }
+                    }
+                }
+            }
+            string[] command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "End")
+            {
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                double value = double.Parse(command[3]);
+
+                if (0 <= row && row < rowCol.Length
+                 && 0 <= col && col < rowCol[row].Length)
+                {
+                    if (command[0] == "Add")
+                    {
+                        rowCol[row][col] += value;
+                    }
+                    else if (command[0] == "Subtract")
+                    {
+                        rowCol[row][col] -= value;
+                    }
+                }
+                command = Console.ReadLine().Split();
+            }
+            Array.ForEach(rowCol, x => Console.WriteLine(string.Join(" ", x)));
+        }
+    }
+}
