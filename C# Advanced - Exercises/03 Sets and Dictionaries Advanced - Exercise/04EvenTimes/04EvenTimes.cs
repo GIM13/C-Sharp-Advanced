dd7b6f61ebc5eccccf8cf38716999f5ed21d42ce
Genadi <gim13@abv.bg>
2019-09-21 16:22:45 +0300

    solution of task 04 from Sets and Dictionaries Advanced - Exercise
     master

---
+++ C# Advanced - Exercises/03 Sets and Dictionaries Advanced - Exercise/04EvenTimes/04EvenTimes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04EvenTimes
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numInput = int.Parse(Console.ReadLine());
+
+            var digits = new Dictionary<int, int>();
+
+            for (int i = 0; i < numInput; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (!digits.ContainsKey(input))
+                {
+                    digits.Add(input,0);
+                }
+                digits[input]++;
+            }
+            foreach (var item in digits)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
