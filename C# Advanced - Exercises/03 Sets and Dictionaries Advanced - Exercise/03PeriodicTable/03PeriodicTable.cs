069295c63dde52e7710d6987c76524c3696b2195
Genadi <gim13@abv.bg>
2019-09-21 15:38:14 +0300

    solution of task 03 from Sets and Dictionaries Advanced - Exercise

---
+++ C# Advanced - Exercises/03 Sets and Dictionaries Advanced - Exercise/03PeriodicTable/03PeriodicTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03PeriodicTable
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numInput = int.Parse(Console.ReadLine());
+            var chemicalCompounds = new SortedSet<string>();
+
+            for (int i = 0; i < numInput; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                foreach (var element in input)
+                {
+                    chemicalCompounds.Add(element);
+                }
+            }
+            Console.WriteLine(string.Join(" ", chemicalCompounds));
+        }
+    }
+}
