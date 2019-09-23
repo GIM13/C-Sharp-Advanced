c3cff01aedcd62812a1ce68e8f4a401a5534f694
Genadi <gim13@abv.bg>
2019-09-23 23:32:49 +0300

    solution of task 02 from Functional Programming - Exercises

---
+++ C# Advanced - Exercises/04FunctionalProgrammingExercises/02KnightsOfHonor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02KnightsOfHonor
+{
+    class Program
+    {
+        static void Main()
+        {
+            Action<string[]> printAddSir = arr => arr
+                             .ToList()
+                             .ForEach(x => Console.WriteLine($"Sir {x}"));
+
+            string[] text = Console.ReadLine().Split();
+
+            printAddSir(text);
+        }
+    }
+}
