0291f975799f8912f84832c996d482391bf46ade
Genadi <gim13@abv.bg>
2019-09-24 00:36:49 +0300

    solution of task 03 from Functional Programming - Exercises

---
+++ C# Advanced - Exercises/04FunctionalProgrammingExercises/03CustomMinFunction/03CustomMinFunction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Func<int[], int> minValue =  arr => arr.Min();
+
+        int[] input = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        Console.WriteLine(minValue(input)); 
+    }
+}
+
