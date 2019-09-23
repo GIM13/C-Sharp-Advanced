07f51e7ab8b69fd5e68d8e2c487cf192a73f6a33
Genadi <gim13@abv.bg>
2019-09-23 20:16:25 +0300

    solution of task 01 from Functional Programming - Exercises

---
+++ C# Advanced - Exercises/04FunctionalProgrammingExercises/01ActionPrint/01ActionPrint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _01ActionPrint
+{
+    class Program
+    {
+        static void Main()
+        {
+            Action<string[]> printArr = arr => arr
+                .ToList()
+                .ForEach(text => Console.WriteLine(text)); 
+
+            string[] collectionStrings = Console.ReadLine().Split();
+
+            printArr(collectionStrings);
+        }
+    }
+}
