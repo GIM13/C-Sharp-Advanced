8d8d42f9ffcc307f6819ee6b788fd050992eab2b
Genadi <gim13@abv.bg>
2019-09-24 17:58:35 +0300

    solutions of tasks 04 and 05 from Functional Programming - Exercises

---
+++ C# Advanced - Exercises/04FunctionalProgrammingExercises/04FindEvensOrOdds/04FindEvensOrOdds.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] range = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+        int start = range[0];
+        int end = range[1];
+
+        string command = Console.ReadLine();
+
+        Predicate<string> oddCom = x => x == "odd";
+        Predicate<int> oddNum = x => x % 2 != 0;
+        Predicate<string> evenCom = x => x == "even";
+        Predicate<int> evenNum = x => x % 2 == 0;
+
+        for (int i = start; i <= end; i++)
+        {
+            if (oddCom(command) && oddNum(i))
+            {
+                Console.Write(i + " ");
+            }
+            if (evenCom(command) && evenNum(i))
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
+
---
+++ C# Advanced - Exercises/04FunctionalProgrammingExercises/05AppliedArithmetics/05AppliedArithmetics.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        Predicate<string> addCom = x => x == "add";
+        Predicate<string> multiplyCom = x => x == "multiply";
+        Predicate<string> subtractCom = x => x == "subtract";
+        Predicate<string> printCom = x => x == "print";
+        Predicate<string> end = x => x == "end";
+
+        Func<int[], int[]> add1 = arr => arr.Select(x => ++x).ToArray();
+        Func<int[], int[]> multiplyTo2 = arr => arr.Select(x => x *= 2).ToArray();
+        Func<int[], int[]> subtract1 = arr => arr.Select(x => --x).ToArray();
+        Action<int[]> print = x => Console.WriteLine(string.Join(" ", x));
+
+        string command;
+        while (!end(command = Console.ReadLine()))
+        {
+            if (addCom(command))
+            {
+                numbers = add1(numbers);
+            }
+            else if (multiplyCom(command))
+            {
+                numbers = multiplyTo2(numbers);
+            }
+            else if (subtractCom(command))
+            {
+                numbers = subtract1(numbers);
+            }
+            else if (printCom(command))
+            {
+                print(numbers);
+            }
+        }
+    }
+}
+
