43ad7eb07b32c4019c86dd6a28bce789a8d31e16
Genadi <gim13@abv.bg>
2019-09-24 22:30:25 +0300

    solution of task 06 from Functional Programming - Exercises

---
+++ C# Advanced - Exercises/04FunctionalProgrammingExercises/06ReverseAndExclude/06ReverseAndExclude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+        int divider = int.Parse(Console.ReadLine());
+
+        Predicate<int> divisibleToDivider = x => x % divider == 0;
+        Func<int[],int[]> removeDivisibleToDivider = x => x
+                                       .Where(z => !divisibleToDivider(z))
+                                       .ToArray();
+        Func<int[],int[]> revers = y => y.Reverse().ToArray();
+
+        numbers = removeDivisibleToDivider(revers(numbers));
+        Console.WriteLine(string.Join(" ",numbers));
+    }
+}
+
