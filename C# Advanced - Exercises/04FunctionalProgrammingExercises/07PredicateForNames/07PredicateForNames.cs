a44465fce01add2a6fd6affa363857c623c47565
Genadi <gim13@abv.bg>
2019-09-26 00:49:59 +0300

    solutions of tasks 07 and 08 from Functional Programming - Exercises

---
+++ C# Advanced - Exercises/04FunctionalProgrammingExercises/07PredicateForNames/07PredicateForNames.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int nameLength = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split();
+
+        Action<string[]> lessThanOrEqual = arr => arr
+                         .Where(x => x.Length <= nameLength)
+                         .ToList()
+                         .ForEach(x => Console.WriteLine(x));
+        lessThanOrEqual(names);
+    }
+}
+
---
+++ C# Advanced - Exercises/04FunctionalProgrammingExercises/08CustomComparator/08CustomComparator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        Func<int[], int[]> even = e => e = numbers
+                                          .Where(x => x % 2 == 0)
+                                          .OrderBy(x => x)
+                                          .ToArray();
+
+        Func<int[], int[]> odd = o => o = numbers
+                                          .Where(x => x % 2 != 0)
+                                          .OrderBy(x => x)
+                                          .ToArray();
+
+        Func<int[], Dictionary<int, int[]>> firstEven = x =>
+                                        new Dictionary<int, int[]>
+                                        { { 1, even(x) }, { 2, odd(x) } };
+
+        Dictionary<int, int[]> result = firstEven(numbers);
+
+        Array.Sort(result.Keys.ToArray());
+
+        foreach (var arr in result)
+        {
+            Console.Write(string.Join(" ", arr.Value) + " ");
+        }
+    }
+}
+
