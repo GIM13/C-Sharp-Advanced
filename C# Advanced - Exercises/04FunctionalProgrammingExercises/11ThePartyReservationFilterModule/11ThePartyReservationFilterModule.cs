ba331f8551e438467f5fc75047984342092780b2
Genadi <gim13@abv.bg>
2019-09-28 16:01:33 +0300

    solutions of tasks 11 and 12 from Functional Programming - Exercises

---
+++ C# Advanced - Exercises/04FunctionalProgrammingExercises/11ThePartyReservationFilterModule/11ThePartyReservationFilterModule.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> guests = Console.ReadLine().Split().ToList();
+        string[] command = Console.ReadLine().Split(";");
+
+        var banished = new Dictionary<string, int>();
+
+        while (command[0] != "Print")
+        {
+            if (command[0] == "Add filter")
+            {
+                for (int i = 0; i < guests.Count; i++)
+                {
+                    if ((command[1] == "Starts with" && guests[i].StartsWith(command[2]))
+                     || (command[1] == "Ends with" && guests[i].EndsWith(command[2]))
+                     || (command[1] == "Length" && guests[i].Count() == int.Parse(command[2]))
+                     || (command[1] == "Contains" && guests[i].Contains(command[2])))
+                    {
+                        if (!banished.ContainsKey(guests[i]))
+                        {
+                            banished.Add(guests[i], i);
+                        }
+                        guests.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+            else if (command[0] == "Remove filter")
+            {
+                foreach (var name in banished)
+                {
+                    if ((command[1] == "Starts with" && name.Key.StartsWith(command[2]))
+                     || (command[1] == "Ends with" && name.Key.EndsWith(command[2]))
+                     || (command[1] == "Length" && name.Key.Count() == int.Parse(command[2]))
+                     || (command[1] == "Contains" && name.Key.Contains(command[2])))
+                    {
+                        if (name.Value < guests.Count)
+                        {
+                            guests.Insert(name.Value, name.Key);
+                        }
+                        else
+                        {
+                            guests.Add(name.Key);
+                        }
+                    }
+                }
+            }
+            command = Console.ReadLine().Split(";");
+        }
+        if (guests.Any())
+        {
+            Console.WriteLine(string.Join(" ", guests));
+        }
+    }
+}
+
+
---
+++ C# Advanced - Exercises/04FunctionalProgrammingExercises/12TriFunction/12TriFunction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int criteria = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split();
+
+        Func<string, int, bool> isItBiggerStringSum = (x, y) => x.ToCharArray()
+                                                       .Select(c => (int)c)
+                                                       .Sum() >= y;
+        Func<string[], Func<string, int, bool>, string[]> accordingCriterion = (name, func) =>
+                                                   name.Where(z => func(z, criteria))
+                                                       .ToArray();
+        string[] result = accordingCriterion(names, isItBiggerStringSum);
+
+        if (result.Any())
+        {
+            Console.WriteLine(result[0]);
+        }
+    }
+}
+
