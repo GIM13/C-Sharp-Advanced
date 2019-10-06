65c6effa0c804573eb2156a4900bb0504c2cbfa8
Genadi <gim13@abv.bg>
2019-10-06 11:23:32 +0300

    solution of task 12 from Functional Programming - Exercises

--- C# Advanced - Exercises/04FunctionalProgrammingExercises/12TriFunction/12TriFunction.cs
+++ C# Advanced - Exercises/04FunctionalProgrammingExercises/12TriFunction/12TriFunction.cs
@@ -8,12 +8,14 @@ class Program
         int criteria = int.Parse(Console.ReadLine());
         string[] names = Console.ReadLine().Split();
 
-        Func<string, int, bool> isItBiggerStringSum = (x, y) => x.ToCharArray()
-                                                       .Select(c => (int)c)
-                                                       .Sum() >= y;
-        Func<string[], Func<string, int, bool>, string[]> accordingCriterion = (name, func) =>
-                                                   name.Where(z => func(z, criteria))
-                                                       .ToArray();
+        Func<string, int, bool> isItBiggerStringSum = (x, y) =>  
+                     x.ToCharArray()
+                      .Select(c => (int)c)
+                      .Sum() >= y;
+        Func<string[], Func<string, int, bool>, string[]> accordingCriterion =
+                     (name, func) => name
+                     .Where(z => func(z, criteria))
+                     .ToArray();
         string[] result = accordingCriterion(names, isItBiggerStringSum);
 
         if (result.Any())
---
+++ C# Advanced - Exercises/05DefiningClassesExercise/DefiningClasses/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+           var firstPerson = new Person();
+           var secondPerson = new Person();
+           var thirdPerson = new Person();
+
+           firstPerson.Name = "Pesho";
+           firstPerson.Age = 20;
+
+           secondPerson.Name = "Gosho";
+           secondPerson.Age = 18;
+
+           thirdPerson.Name = "Stamat";
+           thirdPerson.Age = 43;
+        }
+    }
+}
