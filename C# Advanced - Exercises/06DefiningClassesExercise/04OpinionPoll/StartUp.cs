97e5c3481baae57f07178fd085f7d657e5acd6ea
Genadi <gim13@abv.bg>
2019-10-11 09:21:17 +0300

    solution of task 04 from Defining Classes - Exercise

---
+++ C# Advanced - Exercises/06DefiningClassesExercise/04OpinionPoll/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DefiningClasses;
+
+namespace _04OpinionPoll
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numInfo = int.Parse(Console.ReadLine());
+
+            var people = new List<Person>();
+
+            for (int i = 0; i < numInfo; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                double age = double.Parse(input[1]);
+
+                if (age > 30)
+                {
+                    people.Add(new Person(age, name));
+                }
+            }
+
+            people = people.OrderBy(x => x.Name).ToList();
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
