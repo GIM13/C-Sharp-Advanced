2602b6518470e3699095c5f579197a33479de89d
Genadi <gim13@abv.bg>
2019-10-12 15:36:52 +0300

    solution of task 07 from Defining Classes - Exercise

---
+++ C# Advanced - Exercises/06DefiningClassesExercise/07RawData/Car.cs
@@ -0,0 +1,22 @@
+﻿
+namespace _07RawData
+{
+    class Car
+    {
+        public Car(string model, Engine engine, Cargo cargo, TireSet tireSet)
+        {
+            Model = model;
+            Engine = engine;
+            Cargo = cargo;
+            TireSet = tireSet;
+        }
+
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Cargo Cargo { get; set; }
+
+        public TireSet TireSet { get; set; }
+    }
+}
---
+++ C# Advanced - Exercises/06DefiningClassesExercise/07RawData/Engine.cs
@@ -0,0 +1,16 @@
+﻿
+namespace _07RawData
+{
+    class Engine
+    {
+        public Engine(int engineSpeed, int enginePower)
+        {
+            EngineSpeed = engineSpeed;
+            EnginePower = enginePower;
+        }
+
+        public int EngineSpeed { get; private set; }
+
+        public int EnginePower { get; private set; }
+    }
+}
